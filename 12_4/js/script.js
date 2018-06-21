2314d8ae0bc31864e2a44152d2477e15442fd318
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-06-21 14:36:54 +0200

    function getJoke improved

--- 12_4/js/script.js
+++ 12_4/js/script.js
@@ -1,14 +1,15 @@
-'use strict'
+'use strict';
+
 var url = 'http://api.icndb.com/jokes/random';
 var button = document.getElementById('get-joke');
-button.addEventListener('click', function() {
-	getJoke();
-});
+button.addEventListener('click', getJoke);
 
-function getJoke() {
+var paragraph = document.getElementById('joke');
+
+function getJoke(){
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', url);
-	xhr.addEventListener('load', function() {
+	xhr.addEventListener('load', function(){
 		var response = JSON.parse(xhr.response);
 		paragraph.innerHTML = response.value.joke;
 	});
