fd65c3d43597c8628c8a312ce2f53f88cfaf51c0
Ewelina Kusmierczyk-Flazinska <ewelina@flazinska.pl>
2018-06-25 22:32:31 +0200

    12.6 scriptjs corrected

--- 12_6/script.js
+++ 12_6/script.js
@@ -1,16 +1,13 @@
 var url = 'https://restcountries.eu/rest/v2/name/';
 var countriesList = document.getElementById('countries');
-
 document.getElementById('search').addEventListener('click', searchCountries);
 
 function searchCountries() {
 	var countryName = document.getElementById('country-name').value;
-	if(!countryName.length) countryName = 'Poland';
-	fetch(url + countryName)
-	.then(function(resp) {
+	if (!countryName.length) countryName = 'Poland';
+	fetch(url + countryName).then(function(resp) {
 		return resp.json();
-	})
-	.then(showCountriesList);
+	}).then(showCountriesList);
 }
 
 function showCountriesList(resp) {
@@ -18,7 +15,7 @@ function showCountriesList(resp) {
 	resp.forEach(function(item) {
 		var divEl = document.createElement('div');
 		divEl.classList.add("country");
-		divEl.innerHTML = "Name: " + item.name+ '<br> Flag: <img src="' + item.flag + '"> <br> Capital: ' + item.capital;
+		divEl.innerHTML = "Name: " + item.name + '<br> Flag: <img src="' + item.flag + '"> <br> Capital: ' + item.capital;
 		countriesList.appendChild(divEl);
 	});
 }
